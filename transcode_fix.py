44064cd3344968eec4b098a410ca856cb4056919
Virgiawan <virgiawan@duck.com>
2023-05-10 15:43:25 +0700

    adding script check dir

--- transcode_fix.py
+++ transcode_fix.py
@@ -1,66 +1,73 @@
-import os, glob
+import os
 import ffmpeg
+from ffmpeg import Error
 import datetime as dt
+
 
 INPATH = "/home/virgiawan/BINO/TRANSCODE__/raw"
 TMPPATH = "/home/virgiawan/BINO/TRANSCODE__/tmp"
 OUTPATH = "/home/virgiawan/BINO/TRANSCODE__/compressed"
-LOGPATH = "home/virgiawan/BINO/TRANSCODE__/log/tv"
-PROCPATH = "home/virgiawan/BINO/TRANSCODE__/tv/processed"
-anu = "/home/virgiawan/BINO/VIVIDEO"
+LOGPATH = "/home/virgiawan/BINO/TRANSCODE__/log/tv"
+PROCPATH = "/home/virgiawan/BINO/TRANSCODE__/tv/processed"
+ANU = "/home/virgiawan/BINO/VIVIDEO"
 
 PRESET = "faster"
-CPUCORE = 2
+CPUCORE = 1
 
 VIDEOBITRATE = "512k"
 AUDIOBITRATE = "128k"
 
-TMPMYMAP1 = {}
-TMPMYMAP2 = {}
-SORTFILES1 = {}
-FILES = {}
+# TMPMYMAP1 = {}
+# TMPMYMAP2 = {}
+# SORTFILES1 = {}
+# FILES = {}
+
+if not os.path.exists(INPATH):
+    os.makedirs(INPATH)
+
+elif not os.path.exists(TMPPATH):
+    os.makedirs(TMPPATH)
+
+elif not os.path.exists(OUTPATH):
+    os.makedirs(OUTPATH)
+
+elif not os.path.exists(LOGPATH):
+    os.makedirs(LOGPATH)
+
+elif not os.path.exists(PROCPATH):
+    os.makedirs(PROCPATH)
+
+else:
+    print("== Dir sudah ada ==")
 
 
-# line 37-49
-# if OUTPATH is not None:
-#     os.mkdir(OUTPATH)
+PATTERN = [".mp4", ".ts", ".mpg", ".TS", ".mkv"]
 
-# elif TMPPATH is not None:
-#     os.mkdir(TMPPATH)
 
-# else:
-#     pass
-
-FILES1 = (os.listdir(anu), glob.glob("*.mp4"))
+FILES1 = [file for file in os.listdir(ANU) if file.endswith(tuple(PATTERN))]
 
 
 def run_proc():
     try:
-        for TMPFILE in FILES1:
-            for VIDEO in TMPFILE:
-                INFILE = ffmpeg.input(VIDEO)
-                BASENAME = os.path.basename(VIDEO)
-                OUTFILE = ffmpeg.output(
-                    INFILE,
-                    # OUTPATH,
-                    f"{OUTPATH}{BASENAME}",
-                    loglevel="quiet",
-                    vcodec="libx264",
-                    preset=f"{PRESET}",
-                    threads=f"{CPUCORE}",
-                    vb=f"{VIDEOBITRATE}",
-                    ab=f"{AUDIOBITRATE}",
-                    strict=2,
-                )
-                # print(BASENAME)
+        # for TMPFILE in FILES1:
+        for VIDEO in FILES1:
+            INFILE = ffmpeg.input(VIDEO)
+            BASENAME = os.path.basename(VIDEO)
+            OUTFILE = ffmpeg.output(
+                INFILE,
+                f"{OUTPATH}{BASENAME}",
+                loglevel="quiet",
+                vcodec="libx264",
+                preset=f"{PRESET}",
+                threads=f"{CPUCORE}",
+                vb=f"{VIDEOBITRATE}",
+                ab=f"{AUDIOBITRATE}",
+                strict=2,
+            )
+            ffmpeg.run(OUTFILE, capture_stdout=True, capture_stderr=True)
+            # print(dt.datetime.now(), "--- START ---")
 
-                ffmpeg.run(OUTFILE)
-                print(dt.datetime.now(), "--- START ---")
-
-    except ffmpeg.Error as e:
-        print("stdout FFMpeg Error")
-        print(e.stdout)
-        print("stderr FFMpeg Error")
+    except Error as e:
         print(e.stderr)
 
 
